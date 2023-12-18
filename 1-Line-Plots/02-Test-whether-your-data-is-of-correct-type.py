8a5250d9b52d74184543a4c1ff0cb0a8e4c804b3
Sabin <spradh@users.noreply.github.com>
2023-12-18 17:46:23 -0600

    Create 02-Test-whether-your-data-is-of-correct-type.py

---
+++ 1-Line-Plots/02-Test-whether-your-data-is-of-correct-type.py
@@ -0,0 +1,8 @@
+# Print the data type of each column in discoveries
+print(discoveries.dtypes)
+
+# Convert the date column to a datestamp type
+discoveries['date'] = pd.to_datetime(discoveries['date'])
+
+# Print the data type of each column in discoveries, again
+print(discoveries.dtypes)
