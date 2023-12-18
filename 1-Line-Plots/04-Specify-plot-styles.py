5d2b12fd32badd4a8452f36b89cb561e5147cbd5
Sabin <spradh@users.noreply.github.com>
2023-12-18 17:50:53 -0600

    Create 04-Specify-plot-styles.py

---
+++ 1-Line-Plots/04-Specify-plot-styles.py
@@ -0,0 +1,10 @@
+# Import the matplotlib.pyplot sub-module
+import matplotlib.pyplot as plt
+
+# Use the fivethirtyeight style
+plt.style.use('fivethirtyeight')
+
+# Plot the time series
+ax1 = discoveries.plot()
+ax1.set_title('FiveThirtyEight Style')
+plt.show()
