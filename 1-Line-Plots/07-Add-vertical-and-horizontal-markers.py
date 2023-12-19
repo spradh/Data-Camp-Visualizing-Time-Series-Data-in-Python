89482dea05095d27790e99b3e4f2523ac0c1ec1e
Sabin <spradh@users.noreply.github.com>
2023-12-18 18:10:46 -0600

    Create 07-Add-vertical-and-horizontal-markers.py

---
+++ 1-Line-Plots/07-Add-vertical-and-horizontal-markers.py
@@ -0,0 +1,10 @@
+# Plot your the discoveries time series
+ax = discoveries.plot(color='blue', fontsize=6)
+
+# Add a red vertical line
+ax.axvline('1939-1-1', color = 'red', linestyle='--')
+
+# Add a green horizontal line
+ax.axhline(4, color='green', linestyle='--')
+
+plt.show()
