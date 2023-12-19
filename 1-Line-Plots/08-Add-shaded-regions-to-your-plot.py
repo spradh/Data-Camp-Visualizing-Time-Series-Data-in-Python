ed088b5393276de5b1e8fbc87229b067b7161da2
Sabin <spradh@users.noreply.github.com>
2023-12-18 18:13:24 -0600

    Create 08-Add-shaded-regions-to-your-plot.py

---
+++ 1-Line-Plots/08-Add-shaded-regions-to-your-plot.py
@@ -0,0 +1,10 @@
+# Plot your the discoveries time series
+ax = discoveries.plot(color='blue', fontsize=6)
+
+# Add a vertical red shaded region
+ax.axvspan('1900-01-01', '1915-01-01', color='red', alpha=0.3)
+
+# Add a horizontal green shaded region
+ax.axhspan(6, 8, color='green', alpha=0.3)
+
+plt.show()
