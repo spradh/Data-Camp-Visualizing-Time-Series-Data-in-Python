78b3a7474fa87c95bf30c007d764834d124e655e
Sabin <spradh@users.noreply.github.com>
2023-12-18 21:57:04 -0600

    Create 03-Display-rolling-averages.py

---
+++ 2-Summary-Statistics-and-Diagnostic/03-Display-rolling-averages.py
@@ -0,0 +1,20 @@
+# Compute the 52 weeks rolling mean of the co2_levels DataFrame
+ma = co2_levels.rolling(window=52).mean()
+
+# Compute the 52 weeks rolling standard deviation of the co2_levels DataFrame
+mstd = co2_levels.rolling(window=52).std()
+
+# Add the upper bound column to the ma DataFrame
+ma['upper'] = ma['co2'] + (2 * mstd['co2'])
+
+# Add the lower bound column to the ma DataFrame
+ma['lower'] = ma['co2'] - (2 * mstd['co2'])
+
+# Plot the content of the ma DataFrame
+ax = ma.plot(linewidth=0.8, fontsize=6)
+
+# Specify labels, legend, and show the plot
+ax.set_xlabel('Date', fontsize=10)
+ax.set_ylabel('CO2 levels in Mauai Hawaii', fontsize=10)
+ax.set_title('Rolling mean and variance of CO2 levels\nin Mauai Hawaii from 1958 to 2001', fontsize=10)
+plt.show()
