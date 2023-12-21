aac0b020780f3af19e716d5ac0a86281db640d5c
Sabin <spradh@users.noreply.github.com>
2023-12-21 10:41:18 -0600

    Create 03-Plot-all-the-time-series-in-your-dataset.py

---
+++ 5-Case-Study-Unemployment-Rate/03-Plot-all-the-time-series-in-your-dataset.py
@@ -0,0 +1,16 @@
+# A subset of the jobs DataFrame
+jobs_subset = jobs[['Finance', 'Information', 'Manufacturing', 'Construction']]
+
+# Print the first 5 rows of jobs_subset
+print(jobs.head())
+
+# Create a facetted graph with 2 rows and 2 columns
+ax = jobs_subset.plot(subplots=True,
+                      layout=(2,2),
+                      sharex=False,
+                      sharey=False,
+                      linewidth=0.7,
+                      fontsize=3,
+                      legend=False)
+
+plt.show()
